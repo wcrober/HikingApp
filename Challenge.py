23fe28fcf5fbfb78f78490ac41d2a238f8ab83f3
Will Roberts <wcrober@gmail.com>
2019-06-11 14:07:44 -0500

    updated record-hike results and created schedule post and auto-tract api's

--- Challenge.py
+++ Challenge.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, url_for, jsonify
+import geocoder
+
+
+
 app = Flask(__name__)
 
-
+g = geocoder.ip('me')
+latitude = g.lat
+longitude = g.lng
 
 
 
@@ -10,22 +16,29 @@ def landing():
     return render_template('home.html')
 
 
-
 @app.route('/record-hike')
 def record_hike():
     return render_template('record-hike.html')
 
 
 
-@app.route('/result',methods = ['POST', 'GET'])
+
+@app.route('/result',methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         result = request.form
-        return  render_template('result.html', result = result)
+        return render_template('result.html', result=result,latitude=latitude, longitude=longitude )
 
 
+@app.route('/schedule-post')
+def schedule_post():
+    return render_template('schedule_post.html')
 
-if __name__ ==  '__main__':
+@app.route('/auto-track')
+def autot_track():
+    return render_template('auto_track.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
 
 
